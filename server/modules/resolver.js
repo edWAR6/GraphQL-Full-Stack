2d4b430b73f2d103ce32bea76153f81be2b510c6
Eduardo Oviedo Blanco <eduardo.oviedo@gmail.com>
2019-11-29 22:00:17 -0600

    Starting to work in the client side

--- server/modules/resolver.js
+++ server/modules/resolver.js
@@ -12,4 +12,4 @@ const resolvers = {
   }
 };
 
-module.exports = resolve
+module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -3,13 +3,14 @@ const { ApolloServer } = require('apollo-server-express');
 const mongoose = require('./config/database');
 const typeDefinitions = require('./modules/graphqlSchema');
 const resolvers = require('./modules/resolver');
+const port = process.env.PORT || 3000;
 
-const server = new ApolloServer({ typeDefinitions, resolvers });
+const server = new ApolloServer({ typeDefs: typeDefinitions, resolvers });
 
 const app = express();
 
 server.applyMiddleware({ app });
 
-app.listen({ port: 3000 }, () => {
+app.listen({ port }, () => {
   console.log(`Server running on http://localhost:${port}${server.graphqlPath}`);
 });
